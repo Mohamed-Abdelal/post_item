1de16fcae247beb330f8a859b00a5bb0b9999613
Mohamed-Abdelal <mohamedabdelaal2517@outlook.com>
2024-05-09 02:11:25 +0300

    simple ui for facebook post

---
+++ app/src/main/java/com/example/post_item/Post_Item.java
@@ -0,0 +1,14 @@
+package com.example.post_item;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Post_Item extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_item);
+    }
+}
